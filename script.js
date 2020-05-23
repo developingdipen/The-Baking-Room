523376887161edb45fc48d368557e61bf1fa0131
Dipen Patel <60754812+developingdipen@users.noreply.github.com>
2020-05-23 18:04:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,7 @@ function scrollToDiscover() {
   document.getElementById("discover").scrollIntoView()
 }
 
-
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
